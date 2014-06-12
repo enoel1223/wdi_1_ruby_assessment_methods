d4642dda62327baf0838d6278f6c2b3fe386d08e
Eddie Noel <enoel1223@gmail.com>
2014-06-12 10:37:50 -0400

    Finish the assessment

--- methods.rb
+++ methods.rb
@@ -7,6 +7,7 @@ end
 y = square(2)
 # What is the value of 'x' after this line executes?
 
+x = 4
 
 #### Question 2
 # What is the value of `age` after we call `get_older`?
@@ -18,6 +19,7 @@ end
 age = 10
 get_older(age)
 
+age = 11
 
 #### Question 3
 # What is the return value of `jump` when called on the last line here?
@@ -32,7 +34,7 @@ def jump
 end
 
 energy = 2
-jump
+jump = 2
 
 
 #### Question 4
@@ -40,6 +42,9 @@ jump
 # and returns the original string multiplied that many times (using the * operator).
 # Do not use an options hash or keyword arguments.
 
+def accept(string, multiplier = 3)
+  string * multiplier
+end
 
 #### Question 5
 # Write code using this method to create the string "HELLO THERE, PUNY HUMANS"
@@ -48,3 +53,6 @@ def greeting(name:, salutation: 'Hi', volume: :low)
   greeting = "#{salutation} there, #{name}"
   volume == :high ? greeting.upcase : greeting
 end
+
+
+greeting("Puny Humans", "Hello", volume: :high)
